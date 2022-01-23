a050de40158bb23b4d8ac8004bbb17fd72725e5d
Pavel <mixmaiker@mail.ru>
2022-01-23 23:36:34 +0300

    01 home work

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,28 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b**2-4*a*c;
+
+  if(d === 0) {
+    let x = -b/(2*a);
+    arr.push(x);
+  } else if(d > 0) {
+    let x1 = (-b + Math.sqrt(d) )/(2*a);
+    let x2 = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(x1, x2);
+  }
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+function calculateTotalMortgage(percent, contribution, c, period) {
+  const P = percent/(12*100);
+  const S = c - contribution;
+	let monthlyPayment = S * ((P) + (P) / (((1 + (P))**period) - 1));
+  let totalAmount = parseFloat((period * monthlyPayment).toFixed(2));
   return totalAmount;
 }
+
+console.log(solveEquation(1, -2 , -7));
+
+console.log(calculateTotalMortgage(10, 0, 20000, 24));
